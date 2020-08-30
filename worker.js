7397ae99514034e41d6190d7330f595096633b1b
Leonardo Russo <info@lrusso.com>
2020-08-29 21:30:45 -0300

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,34 @@
+const filesToCache = [
+	"MiniPiano.htm",
+	"MiniPiano.json",
+	"MiniPiano.png",
+	"MiniPianoGame.htm",
+	"MiniPianoGame.js",
+	"MiniPianoFavIcon_16x16.png",
+	"MiniPianoFavIcon_192x192.png",
+	"MiniPianoFavIcon_512x512.png"
+];
+
+const staticCacheName = "minipiano-v1";
+
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open(staticCacheName)
+		.then(cache => {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request)
+		}).catch(error => {
+		})
+	);
+});
